1e98c5f9da61d7cf957e0c08719eb88eeaec73a0
sfo <147263008+sfforrywetto@users.noreply.github.com>
2025-05-21 05:59:11 +0200

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Header />
       <section className="p-8 ">
         <p className="text-2xl font-bold text-[#143d1c]">Introducing my portfolio gallary as Web/Grafik-Designer and IT-Specialist</p>
-        <p className="text-lg font-light">To see my projects click on "Projects" on the top or choose one from introduced bellow </p>
+        <p className="text-lg font-light">To see my projects click on -Projects- on the top or choose one from introduced bellow </p>
         <CardCarousel />
       </section>
       <Footer />
